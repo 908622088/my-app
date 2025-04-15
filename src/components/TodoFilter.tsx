59297abcb5dd0587a00e5cce3224105b82a9306e
mazhiyun <mazhiyun>
2025-04-15 21:41:07 +0800

    修改按钮配色问题

--- src/components/TodoFilter.tsx
+++ src/components/TodoFilter.tsx
@@ -1,24 +1,31 @@
+import { useState } from "react";
+
 interface TodoFilterProps {
     setFilter: (filter: string) => void;
 }
 function TodoFilter({setFilter}:TodoFilterProps) {
+  const [activeFilter, setActiveFilter] = useState('all')
+  const handleFilter = (filter:string) => {
+    setActiveFilter(filter)
+    setFilter(filter);
+  }
   return (
-    <div className="flex flex-wrap gap-2 mt-4 justify-center sm:justify-start">
+    <div className="flex flex-wrap gap-2 mt-4 sm:justify-center md:justify-start">
       <button 
-        onClick={() => setFilter('all')} 
-        className="px-3 py-1.5 sm:px-4 sm:py-2 text-sm sm:text-base bg-gray-500 text-white rounded hover:bg-gray-600 flex-1 sm:flex-none max-w-[30%] sm:max-w-none"
+        onClick={() => handleFilter('all')} 
+        className={`px-3 py-1.5 sm:px-4 sm:py-2 text-sm sm:text-base ${activeFilter === 'all' ? 'bg-[#323131]' : 'bg-[#858181]'}  text-white rounded hover:bg-[#323131] flex-1 sm:flex-none max-w-[30%] sm:max-w-none`}
       >
         全部
       </button>
       <button 
-        onClick={() => setFilter('active')}
-        className="px-3 py-1.5 sm:px-4 sm:py-2 text-sm sm:text-base bg-yellow-500 text-white rounded hover:bg-yellow-600 flex-1 sm:flex-none max-w-[30%] sm:max-w-none"
+        onClick={() => handleFilter('active')}
+        className={`px-3 py-1.5 sm:px-4 sm:py-2 text-sm sm:text-base ${activeFilter === 'active' ? 'bg-[#d46a0d]' : 'bg-[#f59931]'}  text-white rounded hover:bg-[#d46a0d] flex-1 sm:flex-none max-w-[30%] sm:max-w-none`}
       >
         进行中
       </button>
       <button 
-        onClick={() => setFilter('completed')}
-        className="px-3 py-1.5 sm:px-4 sm:py-2 text-sm sm:text-base bg-green-500 text-white rounded hover:bg-green-600 flex-1 sm:flex-none max-w-[30%] sm:max-w-none"
+        onClick={() => handleFilter('completed')}
+        className={`px-3 py-1.5 sm:px-4 sm:py-2 text-sm sm:text-base ${activeFilter === 'completed' ? 'bg-[#0ca148]' : 'bg-[#57ee3c]'} text-white rounded hover:bg-[#0ca148] flex-1 sm:flex-none max-w-[30%] sm:max-w-none`}
       >
         完成
       </button>
